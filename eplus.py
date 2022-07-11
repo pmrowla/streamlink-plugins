ce90c0ec7d03963242635ea9e4d5d1876603d19d
Mozi <29089388+pzhlkj6612@users.noreply.github.com>
2022-07-11 09:23:40 +0800

    eplus: auto refresh cookies before expiration (rewrite) (#11)
    * eplus: auto refresh cookies before expiration
    
    The "EplusSessionUpdater" class is able to automatically refresh cookies
      for Eplus before they expire.
    
    1. Try to get new cookies by a new "HTTPSession()" object in a separate
       thread.
    2. The time for the next attempt to refresh cookies is determined by
       the expiration time of the previously obtained cookies.
    3. Limited retries with binary exponential backoff if error occurred.
    
    Original author: ipid <ipid@users.noreply.github.com>
    
    Co-authored-by: ipid <ipid@users.noreply.github.com>
    Co-authored-by: Peter Rowlands <peter@pmrowla.com>
    Co-authored-by: Mozi <29089388+pzhlkj6612@users.noreply.github.com>
    
    * eplus: embed "EplusSessionUpdater" in prefix of log
    
    It's logger name hierarchy.
    
    * eplus: set thread name from __qualname__
    
    More flexible
    
    * eplus: follow the original code style
    
    Use double qoutes in most cases.
    
    * eplus: remove the old way to refresh session
    
    The old method was fine, but "EplusSessionUpdater" does better,
      especially when it comes to working with VOD streams or handling
      various issues (e.g., network timeout).
    
    * eplus: merge simple condition test in loop
    
    Co-authored-by: Peter Rowlands (변기호) <peter@pmrowla.com>
    
    * eplus: rm unused attr from "EplusHLSStreamWorker"
    
    To make changes as few as possible but necessary, I preserved the unused
      "self._eplus_url" attribute in the "EplusHLSStreamReader" class.
    
    Co-authored-by: ipid <ipid@users.noreply.github.com>
    Co-authored-by: Peter Rowlands <peter@pmrowla.com>

--- eplus.py
+++ eplus.py
@@ -8,12 +8,14 @@ unsupported).
 import logging
 import html
 import re
+import time
+from threading import Thread, Event
 
 from requests.exceptions import HTTPError
 from streamlink.buffers import RingBuffer
 from streamlink.exceptions import StreamError
 from streamlink.plugin import Plugin
-from streamlink.plugin.api import validate, useragents
+from streamlink.plugin.api import validate, useragents, HTTPSession
 from streamlink.stream.hls import HLSStream, HLSStreamReader, HLSStreamWorker
 
 log = logging.getLogger(__name__)
@@ -37,31 +39,124 @@ def _get_eplus_data(session, eplus_url):
     return result
 
 
-class EplusHLSStreamWorker(HLSStreamWorker):
-    def __init__(self, *args, eplus_url=None, **kwargs):
-        super().__init__(*args, **kwargs)
+class EplusSessionUpdater(Thread):
+    """
+    Cookies for Eplus expire after about 1 hour.
+    To keep our live streaming going, we have to refresh them in time,
+    otherwise we may got HTTP 403 and no new stream could be downloaded.
+    """
+
+    def __init__(self, session, eplus_url):
         self._eplus_url = eplus_url
+        self._session = session
+        self._closed = Event()
+        self._retries = 0
+        self._last_expire_timestamp = time.time()
+        self._log = logging.getLogger(f"{__name__}.{self.__class__.__qualname__}")
+
+        super().__init__(name=self.__class__.__qualname__, daemon=True)
+
+    def close(self):
+        if self._closed.is_set():
+            """
+            "close(self)" will be called multiple times during the cleanup process of Streamlink.
+            If Python is about to exit, logging something will raise an exception:
+            > ImportError: sys.meta_path is None, Python is likely shutting down <
+            """
+            return
+
+        self._log.debug("Closing session updater...")
+        self._closed.set()
+
+    def run(self):
+        self._log.debug("Starting session updater...")
+
+        while not self._closed.is_set():
+
+            # Create a new session without cookies and send a request to Eplus url to obtain new cookies.
+            self._log.debug("Refreshing cookies...")
+            try:
+                fresh_response = self._session_duplicator().get(self._eplus_url)
+                self._log.debug(f"Got new cookies: {repr(fresh_response.cookies)}")
+
+                # Filter cookies.
+                # For now, only the "ci_session" cookie is what we don't need, so ignore it.
+                cookie = next(
+                    cookie for cookie in fresh_response.cookies
+                        if cookie.name != "ci_session"
+                        and cookie.expires > time.time()
+                )
+                self._log.debug(
+                    "Found a valid cookie that will expire at "
+                    f"{time.strftime(r'%Y%m%d-%H%M%S%z', time.localtime(cookie.expires))}. "
+                    f"The cookie: {repr(cookie)}"
+                )
+
+                # Update the global session with the new cookies.
+                self._session.http.cookies.clear()
+                self._session.http.cookies.update(fresh_response.cookies)
+
+                self._retries = 0
+                self._last_expire_timestamp = cookie.expires
+
+                # Refresh cookies at most 5 minutes before expiration.
+                wait_sec = (cookie.expires - 5 * 60) - time.time()
+                if wait_sec < 0:
+                    # It's too close! Retry it right away.
+                    wait_sec = 0
+
+                self._log.debug(
+                    "Refreshed cookies. Next attempt will be at about "
+                    f"{time.strftime(r'%Y%m%d-%H%M%S%z', time.localtime(time.time() + wait_sec))}. "
+                )
+
+                self._closed.wait(wait_sec)
+                continue
+
+            except StopIteration as e:
+                # next() exhausted all cookies.
+                self._log.error("No valid cookies found.")
+
+            except Exception as e:
+                self._log.error(f"Failed to refresh cookies: {e}")
+
+            self._retries += 1
+            retry_delay_sec = 2 ** (self._retries - 1)
+
+            if time.time() + retry_delay_sec > self._last_expire_timestamp + 1 * 60 * 60:
+                self._log.error("We have not refreshed cookies in the past hour and will not try again.")
+
+                self.close()
+                return
+
+            self._log.debug(f"We will retry in {retry_delay_sec}s.")
+
+            self._closed.wait(retry_delay_sec)
+            continue
+
+    def _session_duplicator(self):
+        """
+        Make a duplicate of the member "_session" except for cookies.
+        """
+
+        new_session = HTTPSession()
+
+        new_session.proxies = self._session.http.proxies
+        new_session.headers = self._session.http.headers
+        new_session.trust_env = self._session.http.trust_env
+        new_session.verify = self._session.http.verify
+        new_session.cert = self._session.http.cert
+        new_session.timeout = self._session.http.timeout
+
+        return new_session
+
+
+class EplusHLSStreamWorker(HLSStreamWorker):
+    def __init__(self, *args, **kwargs):
+        super().__init__(*args, **kwargs)
 
     def reload_playlist(self):
-        try:
-            return super().reload_playlist()
-        except StreamError as err:
-            rerr = getattr(err, "err", None)
-            if (
-                self._eplus_url
-                and rerr is not None
-                and isinstance(rerr, HTTPError)
-                and rerr.response.status_code == 403
-            ):
-                log.debug("eplus auth rejected, refreshing session")
-                self.session.http.get(
-                    self._eplus_url,
-                    exception=StreamError,
-                    **self.reader.request_params,
-                )
-            else:
-                raise
-        return super().reload_playlist()
+        super().reload_playlist()
 
 
 class EplusHLSStreamReader(HLSStreamReader):
@@ -70,15 +165,21 @@ class EplusHLSStreamReader(HLSStreamReader):
     def __init__(self, *args, eplus_url=None, **kwargs):
         super().__init__(*args, **kwargs)
         self._eplus_url = eplus_url
+        self.session_updater = EplusSessionUpdater(session=self.session, eplus_url=eplus_url)
 
     def open(self):
         buffer_size = self.session.get_option("ringbuffer-size")
         self.buffer = RingBuffer(buffer_size)
         self.writer = self.__writer__(self)
-        self.worker = self.__worker__(self, eplus_url=self._eplus_url)
+        self.worker = self.__worker__(self)
 
         self.writer.start()
         self.worker.start()
+        self.session_updater.start()
+
+    def close(self):
+        super().close()
+        self.session_updater.close()
 
 
 class EplusHLSStream(HLSStream):
